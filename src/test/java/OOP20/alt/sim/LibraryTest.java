aff42e0dc76b84a56bf7823ca0c447a4ce089c7d
Andrea <andrea.severi12@studio.unibo.it>
2021-03-31 21:25:13 +0200

    add blank lines

--- src/test/java/OOP20/alt/sim/LibraryTest.java
+++ src/test/java/OOP20/alt/sim/LibraryTest.java
@@ -11,5 +11,8 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
         System.out.println("Hello, World!");
+        
+        
+        
     }
 }
