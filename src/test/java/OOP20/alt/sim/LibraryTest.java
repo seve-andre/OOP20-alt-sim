001f3dabbe6a93e7ba57f45517f96a7d4a80ef88
Atanasov00 <74316307+Atanasov00@users.noreply.github.com>
2021-03-31 21:08:40 +0200

    Update LibraryTest.java
    add second Hello World

--- src/test/java/OOP20/alt/sim/LibraryTest.java
+++ src/test/java/OOP20/alt/sim/LibraryTest.java
@@ -11,5 +11,6 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
         System.out.println("Hello, World!");
+        System.out.println("Hello, World2!");
     }
 }
