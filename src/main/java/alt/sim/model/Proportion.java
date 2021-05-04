e6848061cae0eb27b16ad12abae1e64ff5bf9300
Andrea <andrea.severi12@studio.unibo.it>
2021-05-04 21:36:58 +0200

    get rid of tab indentation and checkstyle errors

--- src/main/java/alt/sim/model/Proportion.java
+++ src/main/java/alt/sim/model/Proportion.java
@@ -1,90 +1,94 @@
 package alt.sim.model;
 
-import javafx.geometry.Point2D;
+import java.awt.geom.Point2D;
 
 
-/** Class that manage 2 Ratio object for calculate the unknowValue 10:40 = x:50
- * 
- * @author daniel
+/** Class that manage 2 Ratio object for calculate the unknownValue 10:40 = x:50.
+ *
  */
 public class Proportion {
 
-	private Ratio ratioKnowed;
-	private Ratio ratioToKnowed;
-	private Ratio resultOfProportion;
+    private Ratio ratioKnown;
+    private Ratio ratioToKnow;
+    private Ratio resultOfProportion;
 
-	/**
-	 * @param ratioKnowed Ratio of the proportion that we knowed
-	 * @param ratioToKnowed Ratio need to know
-	 */
-	public Proportion(final Ratio ratioKnowed, final Ratio ratioToKnowed) {
-		this.ratioKnowed = ratioKnowed;
-		this.ratioToKnowed = ratioToKnowed;
-		this.resultOfProportion = new Ratio();
-	}
-	
-	/**
-	 * UnknownValue calculation, in this case we have only the antecedent value
-	 */
-	private void calculationWidthAntecedentKnowed() {
-		double antecedentToKnow = Math.floor(ratioToKnowed.getAntecedent());
-		double consequentToKnow = Math.floor((antecedentToKnow * ratioKnowed.getConsequent()) / ratioKnowed.getAntecedent());
+    /**
+     * @param ratioKnown Ratio of the proportion that we knowed
+     * @param ratioToKnow Ratio need to know
+     */
+    public Proportion(final Ratio ratioKnown, final Ratio ratioToKnow) {
+        this.ratioKnown = ratioKnown;
+        this.ratioToKnow = ratioToKnow;
+        this.resultOfProportion = new Ratio();
+    }
 
-		if(antecedentToKnow == Double.POSITIVE_INFINITY || consequentToKnow == Double.POSITIVE_INFINITY){
-			antecedentToKnow = 0;
-			consequentToKnow = 0;
-		}
+    /**
+     * UnknownValue calculation, in this case we have only the antecedent value.
+     */
+    private void calculationWidthAntecedentKnowed() {
+        double antecedentToKnow = Math.floor(ratioToKnow.getAntecedent());
+        double consequentToKnow = Math.floor(
+                (antecedentToKnow * ratioKnown.getConsequent()) / ratioKnown.getAntecedent()
+        );
 
-		this.resultOfProportion.setAntecedent(antecedentToKnow);
-		this.resultOfProportion.setConsequent(consequentToKnow);
-	}
+        if (antecedentToKnow == Double.POSITIVE_INFINITY || consequentToKnow == Double.POSITIVE_INFINITY) {
+            antecedentToKnow = 0;
+            consequentToKnow = 0;
+        }
 
-	/**
-	 * UnknownValue calculation, in this case we have only the consequent value
-	 * 
-	 * Double.isNan() check if result was divided for 0, causing the NotANumber value
-	 */
-	private void calculationWidthConsequentKnowed() {
-		double consequentToKnow = Math.floor(ratioToKnowed.getConsequent());
-		double antecedentToKnow = Math.floor((consequentToKnow * ratioKnowed.getAntecedent()) / ratioKnowed.getConsequent());
+        this.resultOfProportion.setAntecedent(antecedentToKnow);
+        this.resultOfProportion.setConsequent(consequentToKnow);
+    }
 
-		if(Double.isNaN(antecedentToKnow) == true || Double.isNaN(consequentToKnow) == true){
-			antecedentToKnow = 0;
-			consequentToKnow = 0;
-		}
-		
-		this.resultOfProportion.setAntecedent(antecedentToKnow);
-		this.resultOfProportion.setConsequent(consequentToKnow);
-	}
-	
-	/**
-	 * Summary method that regroup the 2 type of UnknowValue calculation
-	 */
-	public void calculationProportion() {
-		if(this.ratioToKnowed.isAntedentKnowed() == true) {
-			this.calculationWidthAntecedentKnowed();
-		}
-		else { this.calculationWidthConsequentKnowed(); }
-	}
-	
-	/**
-	 * @return Return the resultOfProportion object, it contains the 2 UnknowValues researched
-	 */
-	public Ratio getResultOfProportion() {
-		return this.resultOfProportion;
-	}
-	
-	/**
-	 * @return Return the resultOfProportion object converted for the Point2D type
-	 */
-	public Point2D getResultOfProportionInConvertedType() {
-		Point2D conversionType;
-		double antecedent = this.resultOfProportion.getAntecedent();
-		double consequent = this.resultOfProportion.getConsequent();
-		
-		conversionType = new Point2D(antecedent, consequent);
-		
-		return conversionType;
-	}
+    /**
+     * UnknownValue calculation, in this case we have only the consequent value.
+     *
+     * Double.isNan() check if result was divided for 0, causing the NotANumber value.
+     */
+    private void calculationWidthConsequentKnowed() {
+        double consequentToKnow = Math.floor(ratioToKnow.getConsequent());
+        double antecedentToKnow = Math.floor(
+                (consequentToKnow * ratioKnown.getAntecedent()) / ratioKnown.getConsequent()
+        );
+
+        if (Double.isNaN(antecedentToKnow) || Double.isNaN(consequentToKnow)) {
+            antecedentToKnow = 0;
+            consequentToKnow = 0;
+        }
+
+        this.resultOfProportion.setAntecedent(antecedentToKnow);
+        this.resultOfProportion.setConsequent(consequentToKnow);
+    }
+
+    /**
+     * Summary method that regroup the 2 type of UnknownValue calculation.
+     */
+    public void calculationProportion() {
+        if (this.ratioToKnow.isAntedentKnowed()) {
+            this.calculationWidthAntecedentKnowed();
+        } else {
+            this.calculationWidthConsequentKnowed();
+        }
+    }
+
+    /**
+     * @return Return the resultOfProportion object, it contains the 2 UnknowValues researched
+     */
+    public Ratio getResultOfProportion() {
+        return this.resultOfProportion;
+    }
+
+    /**
+     * @return Return the resultOfProportion object converted for the Point2D type
+     */
+    public Point2D getResultOfProportionInConvertedType() {
+        Point2D conversionType;
+        double antecedent = this.resultOfProportion.getAntecedent();
+        double consequent = this.resultOfProportion.getConsequent();
+
+        conversionType = new Point2D(antecedent, consequent);
+
+        return conversionType;
+    }
 
 }
