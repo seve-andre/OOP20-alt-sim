238e6046f8ad43890444a259aef134e6f3151d4b
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-05-07 13:40:49 +0200

    add interface point2D

---
+++ src/main/java/alt/sim/model/Point2D.java
@@ -0,0 +1,19 @@
+package alt.sim.model;
+
+public interface Point2D {
+    /**
+     * 
+     * @return x-position of Point2D
+     */
+    double getX();
+    /**
+     * 
+     * @return y-position of Point2D
+     */
+    double getY();
+    /**
+     * 
+     * @return the Point2D itself
+     */
+    Point2D getPoint2D();
+}
