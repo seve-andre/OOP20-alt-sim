a10951cfb576f6842ba87ffa2a4c44f36d61a656
Andrea <andrea.severi12@studio.unibo.it>
2021-05-10 16:32:30 +0200

    change multiplatform package

--- src/main/java/alt/sim/model/MultiplatformUtility.java
+++
@@ -1,9 +0,0 @@
-package alt.sim.model;
-
-import java.io.File;
-
-public final class MultiplatformUtility {
-    public static final String KERNEL_SEPARATOR = File.separator;
-
-    private MultiplatformUtility() { }
-}
---
+++ src/main/java/alt/sim/utility/MultiplatformUtility.java
@@ -0,0 +1,18 @@
+package alt.sim.utility;
+
+import java.io.File;
+
+/**
+ *
+ * Utility class to ensure multi-platform software.
+ *
+ */
+public final class MultiplatformUtility {
+
+    /**
+     * Get OS file separator.
+     */
+    public static final String SEPARATOR = File.separator;
+
+    private MultiplatformUtility() { }
+}
