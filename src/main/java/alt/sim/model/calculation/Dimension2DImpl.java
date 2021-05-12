a1cc64a0975bfee9573c19b49de421b3b0a432fa
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-05-12 14:36:45 +0200

    delete redundant files and reuse javafx ones

--- src/main/java/alt/sim/model/calculation/Dimension2DImpl.java
+++ src/main/java/alt/sim/model/calculation/Dimension2DImpl.java
@@ -1,96 +1,39 @@
 package alt.sim.model.calculation;
 
-public class Dimension2DImpl implements Dimension2D {
+import javafx.geometry.Dimension2D;
+import javafx.geometry.Point2D;
+
+public class Dimension2DImpl extends javafx.geometry.Dimension2D {
 
     private static final double HALF = 2;
-    private final double width;
-    private final double height;
-
     /**
      *
      * @param witdh the width of Dimension2D object
      * @param height the height of Dimension2D object
      */
     public Dimension2DImpl(final double witdh, final double height) {
-        this.width = witdh;
-        this.height = height;
+        super(witdh, height);
     }
 
-    @Override
     /**
-     * {@inheritDoc}
-     */
-    public double getWidth() {
-        return this.width;
-    }
-
-    @Override
-    /**
-     * {@inheritDoc}
-     */
-    public double getHeight() {
-        return this.height;
-    }
-
-    @Override
-    /**
-     * {@inheritDoc}
+     * method that calculates the area of the rectangle.
+     * @return the area calculated
      */
     public double computeArea() {
-        return this.height * this.width;
+        return super.getHeight() * super.getWidth();
     }
 
-    @Override
     /**
      * {@inheritDoc}
      */
     public Dimension2D getDimension2D() {
-        return new Dimension2DImpl(this.width, this.height);
+        return new Dimension2DImpl(super.getWidth(), super.getHeight());
     }
 
-    @Override
     /**
      * {@inheritDoc}
      */
     public Point2D computeCenter() {
-        return new Point2DImpl(this.width / HALF, this.height / HALF);
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        long temp;
-        temp = Double.doubleToLongBits(height);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(width);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Dimension2DImpl other = (Dimension2DImpl) obj;
-        if (Double.doubleToLongBits(height) != Double.doubleToLongBits(other.height)) {
-            return false;
-        }
-        if (Double.doubleToLongBits(width) != Double.doubleToLongBits(other.width)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Dimension2D [width=" + width + ", height=" + height + "]";
+        return new Point2D(super.getWidth() / HALF, super.getHeight() / HALF);
     }
 }
