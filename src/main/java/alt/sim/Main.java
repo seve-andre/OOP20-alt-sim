d29d9d5772b39fcbe7599d60c1bb4d20a1d9d3ec
Andrea <andrea.severi12@studio.unibo.it>
2021-05-14 15:49:40 +0200

    add main method

---
+++ src/main/java/alt/sim/Main.java
@@ -0,0 +1,16 @@
+package alt.sim;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(final String[] args) {
+
+    }
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+
+    }
+}
