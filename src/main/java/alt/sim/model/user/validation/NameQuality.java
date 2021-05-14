97979a5fef24f571fc9665fb4e0335a467651bb0
Andrea <andrea.severi12@studio.unibo.it>
2021-05-14 10:25:45 +0200

    refactoring

--- src/main/java/alt/sim/model/user/validation/NameQuality.java
+++ src/main/java/alt/sim/model/user/validation/NameQuality.java
@@ -13,19 +13,15 @@ public final class NameQuality {
      */
     private final String regex = "^[A-Za-z0-9]{1,12}$";
     private final Pattern pattern = Pattern.compile(regex);
-    private String name;
     private static final int MAX_LENGTH = 12;
-
-    public NameQuality(final String name) {
-        this.name  = name;
-    }
 
     /**
      * Checks given name according to regex pattern.
      *
+     * @param name to check for quality
      * @return enum value result
      */
-    public NameValidation checkName() {
+    public NameValidation checkName(final String name) {
 
         final String trimmedName = name.trim();
         if (name.isBlank()) {
