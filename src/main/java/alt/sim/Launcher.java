a214dadba05895c8d5834553ca7e5bd26b0c1310
Andrea <andrea.severi12@studio.unibo.it>
2021-05-19 00:01:29 +0200

    create launcher

---
+++ src/main/java/alt/sim/Launcher.java
@@ -0,0 +1,13 @@
+package alt.sim;
+
+import java.io.IOException;
+
+public final class Launcher {
+
+    private Launcher() {
+    }
+
+    public static void main(final String[] args) throws IOException {
+        Main.main(args);
+    }
+}
