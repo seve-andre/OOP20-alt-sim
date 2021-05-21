9390db620ab5b17f8e677cbb0083bfb39941c102
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-05-21 15:13:49 +0200

    bugfix

--- src/main/java/alt/sim/view/airstrip/BaicAirStripView.java
+++ src/main/java/alt/sim/view/airstrip/BaicAirStripView.java
@@ -12,7 +12,7 @@ public class BaicAirStripView extends Application {
     private static final double SCREEN_WIDTH = Screen.getPrimary().getBounds().getWidth();
     /** Screen height of the view.  */
     private static final double SCREEN_HEIGHT = Screen.getPrimary().getBounds().getHeight();
-    private static final double SCREEN_PROPORTION = 0.6;
+    private static final double SCREEN_PROPORTION = 0.8;
 
     @Override
     public void start(final Stage primaryStage) throws Exception {
@@ -21,10 +21,10 @@ public class BaicAirStripView extends Application {
             ImageResized planeAirStripResized = new ImageResized("images/map_components/airstrip.png");
 
             // View Plane demonstrating:
-            paneRoot.resize(SCREEN_WIDTH * SCREEN_PROPORTION, SCREEN_HEIGHT * SCREEN_PROPORTION);
+            paneRoot.resize(SCREEN_WIDTH, SCREEN_HEIGHT);
             paneRoot.getChildren().add(planeAirStripResized.getImageSprite());
 
-            Scene scene = new Scene(paneRoot, SCREEN_WIDTH, SCREEN_HEIGHT);
+            Scene scene = new Scene(paneRoot, SCREEN_WIDTH * SCREEN_PROPORTION,  SCREEN_HEIGHT * SCREEN_PROPORTION);
             primaryStage.setScene(scene);
             primaryStage.setResizable(false);
             primaryStage.show();
