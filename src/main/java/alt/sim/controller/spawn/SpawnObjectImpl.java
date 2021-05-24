e5741fce5318dde8b5b9a488c364160ec13b1f24
Atanasov Atanas Todorov <atanastodor.atanasov@studio.unibo.it>
2021-05-24 16:07:00 +0200

    Update SpawnObjectImpl.java
    implement  SpawnObjectImpl

--- src/main/java/alt/sim/controller/spawn/SpawnObjectImpl.java
+++ src/main/java/alt/sim/controller/spawn/SpawnObjectImpl.java
@@ -1,17 +1,42 @@
 package alt.sim.controller.spawn;
 
 public class SpawnObjectImpl implements SpawnObject {
-
+    private static final int SPAWN_FREQUENCY = 10000;
+    private static final int OBJECT_NUMBER = 20;
+    private boolean spawned;
+    private int objects;
+    
+    public SpawnObjectImpl() {
+        spawned = false;
+        objects = 0;
+    }
     @Override
     public void startSpawn() {
-        // TODO Auto-generated method stub
-
+         class SpawnRunner implements Runnable{
+            @Override
+            public void run() {
+                while(objects < OBJECT_NUMBER) {
+                    spawnGameObject();
+                    try {
+                        Thread.sleep(SPAWN_FREQUENCY);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+         }
+         Thread threadSpawn = new Thread(new SpawnRunner());
+         threadSpawn.start();
     }
 
     @Override
     public void spawnGameObject() {
-        // TODO Auto-generated method stub
-
+        objects++;
+    }
+    @Override
+    public boolean checkGameObjects() {
+        return false;
     }
 
 }
+
