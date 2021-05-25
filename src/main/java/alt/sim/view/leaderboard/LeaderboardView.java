1446d5470a26fdec0d1d7b6d145f27ec784257a2
Andrea <andrea.severi12@studio.unibo.it>
2021-05-25 21:33:04 +0200

    dirty view just for testing (needs refactoring)

--- src/main/java/alt/sim/view/leaderboard/LeaderboardView.java
+++ src/main/java/alt/sim/view/leaderboard/LeaderboardView.java
@@ -1,16 +1,51 @@
 package alt.sim.view.leaderboard;
 
+import java.util.List;
+
+import alt.sim.model.leaderboard.Leaderboard;
 import alt.sim.view.CommonView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 
 public class LeaderboardView {
 
+    @FXML
+    private TextField nameFirstPlace = new TextField();
+    @FXML
+    private TextField nameSecondPlace = new TextField();
+    @FXML
+    private TextField nameThirdPlace = new TextField();
+    @FXML
+    private TextField nameFourthPlace = new TextField();
+    @FXML
+    private TextField nameFifthPlace = new TextField();
+
+    @FXML
+    private TextField pointsFirstPlace = new TextField();
+    @FXML
+    private TextField pointsSecondPlace = new TextField();
+    @FXML
+    private TextField pointsThirdPlace = new TextField();
+    @FXML
+    private TextField pointsFourthPlace = new TextField();
+    @FXML
+    private TextField pointsFifthPlace = new TextField();
+
+    private final List<String> names = Leaderboard.getTopFive();
 
     @FXML
     public void initialize() {
+        try {
+            nameFirstPlace.setText(names.get(0));
+            nameSecondPlace.setText(names.get(1));
+            nameThirdPlace.setText(names.get(2));
+            nameFourthPlace.setText(names.get(3));
+            nameFifthPlace.setText(names.get(4));
+        } catch (final IndexOutOfBoundsException e) {
 
+        }
     }
 
     @FXML
