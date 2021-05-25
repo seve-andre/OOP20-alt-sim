0158f15be4ac333495976793f325c28328935237
Andrea <andrea.severi12@studio.unibo.it>
2021-05-25 23:14:37 +0200

    add points to view

--- src/main/java/alt/sim/view/leaderboard/LeaderboardView.java
+++ src/main/java/alt/sim/view/leaderboard/LeaderboardView.java
@@ -1,8 +1,10 @@
 package alt.sim.view.leaderboard;
 
 import java.util.List;
+import java.util.Map;
 
 import alt.sim.model.leaderboard.Leaderboard;
+import alt.sim.model.user.records.UserRecordsImpl;
 import alt.sim.view.CommonView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -34,15 +36,25 @@ public class LeaderboardView {
     private TextField pointsFifthPlace = new TextField();
 
     private final List<String> names = Leaderboard.getTopFive();
+    private final Map<String, Integer> users = new UserRecordsImpl().getUsers();
 
     @FXML
     public void initialize() {
         try {
             nameFirstPlace.setText(names.get(0));
+            pointsFirstPlace.setText(users.get(names.get(0)).toString());
+
             nameSecondPlace.setText(names.get(1));
+            pointsSecondPlace.setText(users.get(names.get(1)).toString());
+
             nameThirdPlace.setText(names.get(2));
+            pointsThirdPlace.setText(users.get(names.get(2)).toString());
+
             nameFourthPlace.setText(names.get(3));
+            pointsFourthPlace.setText(users.get(names.get(3)).toString());
+
             nameFifthPlace.setText(names.get(4));
+            pointsFifthPlace.setText(users.get(names.get(4)).toString());
         } catch (final IndexOutOfBoundsException e) {
 
         }
