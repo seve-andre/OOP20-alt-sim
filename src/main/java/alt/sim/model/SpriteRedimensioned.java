ea2041809a2a41e856ff7fe6fda6bb26726ac225
Daniel Rodilosso <daniel.rodilosso@outlook.com>
2021-05-28 16:06:00 +0200

    Create SpriteRedimensioned.java to replace ProportionImage.java

---
+++ src/main/java/alt/sim/model/SpriteRedimensioned.java
@@ -0,0 +1,117 @@
+package alt.sim.model;
+
+public class SpriteRedimensioned {
+    //Pane section:
+    /** Min range of Screen dimension. */
+    public static final double DIMENSION_MIN_RANGE = 60;
+    /** Max range of Screen dimension.  */
+    public static final double DIMENSION_MAX_RANGE = 2560;
+
+    //Sprite section:
+    /** Max range of Sprite dimension. */
+    public static final double SPRITE_MAX_RANGE = 512;
+    /** Min range of Sprite dimension. */
+    public static final double SPRITE_MIN_RANGE = 16;
+
+    private RatioImpl ratioScreen;
+    private RatioImpl ratioSprite;
+    private RatioImpl resultBoundsSprite;
+    private boolean selectTheHightValueResize;
+
+    /**
+     * Constructor initial method.
+    */
+    public SpriteRedimensioned() {
+        this.ratioSprite = new RatioImpl();
+        this.ratioScreen = new RatioImpl();
+        this.resultBoundsSprite = new RatioImpl();
+        this.selectTheHightValueResize = false;
+    }
+
+    public SpriteRedimensioned(final RatioImpl ratioScreen, final RatioImpl ratioSprite, final double isSelectTheHightValueResize) {
+        this();
+        this.ratioScreen = ratioScreen;
+        this.ratioSprite = ratioScreen;
+    }
+
+    // l'immagine da scalare viene calcolata sulla dimensione della Mappa
+    public void scaleSprite(final int percentageScale) {
+        double spriteWidthScaled = UtilityCalculation.persentageCalculation(ratioScreen.getAntecedent(), percentageScale);
+        double spriteHeightScaled = UtilityCalculation.persentageCalculation(ratioScreen.getConsequent(), percentageScale);
+
+        System.out.println("scaleSprite(): " + spriteWidthScaled + " , " + spriteHeightScaled);
+        this.ratioSprite.setRatioValue(spriteWidthScaled, spriteHeightScaled);
+    }
+
+    public void selectSpriteBoundsLow() {
+        int exponent = 0;
+
+        if (ratioSprite.getAntecedent() <= ratioSprite.getConsequent()) {
+            exponent = UtilityCalculation.exponentCalcultation((int) ratioSprite.getAntecedent());
+        } else {
+            exponent = UtilityCalculation.exponentCalcultation((int) ratioSprite.getConsequent());
+        }
+
+        // Calcolo domensione finale dall'esponente
+        this.resultBoundsSprite.setAntecedent(Math.pow(UtilityCalculation.BASE_POWER, exponent));
+        this.resultBoundsSprite.setConsequent(Math.pow(UtilityCalculation.BASE_POWER, exponent));
+    }
+
+    public void selectSpriteBoundsHight() {
+        int exponent = 0;
+
+        if (ratioSprite.getAntecedent() >= ratioSprite.getConsequent()) {
+            exponent = UtilityCalculation.exponentCalcultation((int) ratioSprite.getAntecedent());
+        } else {
+            exponent = UtilityCalculation.exponentCalcultation((int) ratioSprite.getConsequent());
+        }
+
+        // Calcolo domensione finale dall'esponente
+        this.resultBoundsSprite.setAntecedent(Math.pow(UtilityCalculation.BASE_POWER, exponent));
+        this.resultBoundsSprite.setConsequent(Math.pow(UtilityCalculation.BASE_POWER, exponent));
+    }
+
+
+    public void resizedSelectedLowBounds() {
+        scaleSprite(UtilityCalculation.PERCENT_VALUE);
+
+        if (this.selectTheHightValueResize) {
+            selectSpriteBoundsHight();
+        } else {
+            selectSpriteBoundsLow();
+        }
+    }
+
+    public void setRatioScreenValue(final double widthScreen, final double heightScreen) {
+        this.ratioScreen.setAntecedent(widthScreen);
+        this.ratioScreen.setConsequent(heightScreen);
+    }
+
+    public void setRatioSpriteValue(final double widthSprite, final double heightSprite) {
+        this.ratioSprite.setAntecedent(widthSprite);
+        this.ratioSprite.setConsequent(heightSprite);
+    }
+
+    public RatioImpl getResultBoundsSprite() {
+        return this.resultBoundsSprite;
+    }
+
+    public void setSelectTheHightValueResize(final boolean selectTheHightValueResize) {
+        this.selectTheHightValueResize = selectTheHightValueResize;
+    }
+
+    public boolean getSelectTheHightValueResize() {
+        return this.selectTheHightValueResize;
+    }
+
+    public static void main(final String[] args) {
+        SpriteRedimensioned sprtTest = new SpriteRedimensioned();
+
+        sprtTest.setRatioSpriteValue(153, 288);
+        sprtTest.setRatioScreenValue(1920, 1024);
+        sprtTest.setSelectTheHightValueResize(false);
+
+        sprtTest.resizedSelectedLowBounds();
+        //System.out.println("Sprite resized width: " + sprtTest.getResultBoundsSprite().getAntecedent() + " ,height: " + sprtTest.getResultBoundsSprite().getConsequent());
+    }
+}
