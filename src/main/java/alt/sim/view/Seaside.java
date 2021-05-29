2ce81eaa3ad89cb9809a106adfa5ea9260de27c3
Andrea <andrea.severi12@studio.unibo.it>
2021-05-29 17:20:08 +0200

    add pause method for seaside

---
+++ src/main/java/alt/sim/view/Seaside.java
@@ -0,0 +1,18 @@
+package alt.sim.view;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class Seaside {
+
+    @FXML
+    public void initialize() throws IOException {
+
+    }
+
+    @FXML
+    public void onPauseClick() throws IOException {
+        CommonView.onPauseClick();
+    }
+}
