e114b46b88ce4b37a363d106b3cd0dcaa0bf3c51
Daniel Rodilosso <daniel.rodilosso@outlook.com>
2021-05-30 14:49:23 +0200

    Clean Up Plane class

--- src/main/java/alt/sim/model/plane/Plane.java
+++ src/main/java/alt/sim/model/plane/Plane.java
@@ -1,5 +1,6 @@
 package alt.sim.model.plane;
 
+import alt.sim.model.ImageClassification;
 import alt.sim.model.calculation.Sprite;
 import javafx.geometry.Point2D;
 import javafx.scene.image.ImageView;
@@ -25,12 +26,12 @@ public class Plane {
     private State status;
     private Sprite spritePlane;
 
-    public Plane() {
-        this.spritePlane = new Sprite(new Point2D(0, 0));
+    public Plane(final String urlImagePlane) {
+       this.spritePlane = new Sprite(urlImagePlane, true);
     }
 
-    public Plane(final String urlImagePlane) {
-       this.spritePlane = new Sprite(urlImagePlane);
+    public Plane(final ImageClassification imageClassification) {
+        this(imageClassification.getURLImage());
     }
 
     public Plane(final String urlImagePlane, final Point2D point) {
@@ -44,7 +45,6 @@ public class Plane {
      * @param status defined the Plane state.
      */
     public Plane(final Tipology type, final State status) {
-        this();
         this.type = type;
         this.status = status;
     }
@@ -79,7 +79,7 @@ public class Plane {
 
     @Override
     public String toString() {
-        return "Plane [type=" + type + ", status=" + status + "]";
+        return ("Plane [type=" + type + ", status=" + status + "]");
     }
 
 }
