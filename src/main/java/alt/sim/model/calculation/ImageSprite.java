2262c84969f7b3a020a433f579803285171790fc
Daniel Rodilosso <daniel.rodilosso@outlook.com>
2021-05-31 12:12:20 +0200

    Update name class ImageResized --> ImageSprite

---
+++ src/main/java/alt/sim/model/calculation/ImageSprite.java
@@ -0,0 +1,79 @@
+package alt.sim.model.calculation;
+
+import alt.sim.model.SpriteRedimensioned;
+import alt.sim.view.MainPlaneView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * Class for managing the Image of the Plane and calculate his ProportionImage.
+ */
+public class ImageSprite {
+
+    /** URL Path of the image to load. */
+    //private static String urlSprite;
+    private final String urlSprite;
+
+    private SpriteRedimensioned proportionImageResized;
+    private Image loadImage;
+    private ImageView imageSprite;
+    private double widthImage;
+    private double heightImage;
+
+    /**
+     * Initializes a newly created ImageResized object.
+     * @param url
+     */
+    public ImageSprite(final String url) {
+        this.urlSprite = (url);
+
+        this.loadImage = new Image(urlSprite);
+        this.imageSprite = new ImageView(loadImage);
+
+        this.widthImage = loadImage.getWidth();
+        this.heightImage = loadImage.getHeight();
+
+        proportionImageResized = new SpriteRedimensioned();
+        proportionImageResized.setRatioSpriteValue(widthImage, heightImage);
+        proportionImageResized.setRatioScreenValue(MainPlaneView.getScreenWidth(), MainPlaneView.getScreenWidth());
+    }
+
+    public ImageSprite(final String url, final double screenWidth, final double screenHeight, final boolean isPreserveRatio) {
+        this(url);
+    }
+
+    /**
+     * executes the renderingProportionImage() method to apply the resize calculation for this ImageView values.
+     * after do that, it update the width and height values of imageSprite.
+     */
+    public void resizeImageSprite() {
+        double widthResized = 0;
+        double heightResized = 0;
+
+        this.proportionImageResized.resizeBoundsSprite();
+        widthResized = proportionImageResized.getResultBoundsSprite().getAntecedent();
+        heightResized = proportionImageResized.getResultBoundsSprite().getConsequent();
+
+        this.imageSprite.setFitWidth(widthResized);
+        this.imageSprite.setFitHeight(heightResized);
+    }
+
+    /**
+     * @return the imageSprite proportioned to the Screen.
+     */
+    public ImageView getImageSprite() {
+        return this.imageSprite;
+    }
+
+    public String getUrlSprite() {
+        return this.urlSprite;
+    }
+
+    public double getWidthImage() {
+        return this.widthImage;
+    }
+
+    public double getHeightImage() {
+        return this.heightImage;
+    }
+}
