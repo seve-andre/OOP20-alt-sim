0fc98c1eaa00b954471fb38bc362674201ac186c
Andrea <andrea.severi12@studio.unibo.it>
2021-06-01 21:42:25 +0200

    create map choice controller

--- src/main/java/alt/sim/controller/mapchoice/MapChoiceController.java
+++ src/main/java/alt/sim/controller/mapchoice/MapChoiceController.java
@@ -1,10 +1,30 @@
 package alt.sim.controller.mapchoice;
 
+import java.io.IOException;
+
+import alt.sim.model.user.validation.NameValidation;
+
 public interface MapChoiceController {
 
     /**
-     *
+     * Checks given name quality.
      * @param name
+     * @return name quality result
      */
-    void logUser(String name);
+    NameValidation checkName(String name);
+
+    /**
+     * Checks if given name is taken.
+     * @param name
+     * @return true if name is already taken
+     * @throws IOException
+     */
+    boolean isNameTaken(String name) throws IOException;
+
+    /**
+     * Adds given user to file.
+     * @param name
+     * @throws IOException
+     */
+    void addUser(String name) throws IOException;
 }
--- src/main/java/alt/sim/controller/mapchoice/MapChoiceControllerImpl.java
+++ src/main/java/alt/sim/controller/mapchoice/MapChoiceControllerImpl.java
@@ -1,14 +1,40 @@
 package alt.sim.controller.mapchoice;
 
+import java.io.IOException;
+
+import alt.sim.model.user.UserImpl;
+import alt.sim.model.user.records.UserRecordsImpl;
 import alt.sim.model.user.validation.NameQuality;
+import alt.sim.model.user.validation.NameValidation;
 
 public class MapChoiceControllerImpl implements MapChoiceController {
 
-    private final NameQuality nameQuality = new NameQuality();
+    private NameQuality nameQuality = new NameQuality();
+    private UserRecordsImpl userRecordsImpl = new UserRecordsImpl();
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void logUser(final String name) {
-        this.nameQuality.checkName(name);
+    public NameValidation checkName(final String name) {
+        return nameQuality.checkName(name);
     }
 
+    /**
+     * {@inheritDoc}
+     * @throws IOException
+     */
+    @Override
+    public boolean isNameTaken(final String name) throws IOException {
+        return userRecordsImpl.isPresent(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws IOException
+     */
+    @Override
+    public void addUser(final String name) throws IOException {
+        userRecordsImpl.addUser(new UserImpl(name, 0));
+    }
 }
