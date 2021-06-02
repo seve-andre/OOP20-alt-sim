12aceca485e869283104263dc62292a617a2dcd2
Andrea <andrea.severi12@studio.unibo.it>
2021-06-02 15:24:32 +0200

    initialize map with name

--- src/main/java/alt/sim/view/Seaside.java
+++ src/main/java/alt/sim/view/Seaside.java
@@ -1,14 +1,24 @@
 package alt.sim.view;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
 
+import alt.sim.model.user.records.UserRecordsImpl;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class Seaside {
 
     @FXML
-    public void initialize() throws IOException {
+    private TextField name = new TextField();
+    @FXML
+    private TextField score = new TextField();
+    private UserRecordsImpl userRecords = new UserRecordsImpl();
 
+    @FXML
+    public void initialize() throws IOException {
+        final LinkedHashMap<String, Integer> users = this.userRecords.getUsers();
+        name.setText((String) users.keySet().toArray()[users.size() - 1]);
     }
 
     @FXML
