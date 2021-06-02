f306347d84105e627cfbdb19e3ed8b3b6f1980e4
Andrea <andrea.severi12@studio.unibo.it>
2021-06-02 10:35:21 +0200

    Update UserRecordsTest.java

--- src/test/java/alt/sim/model/user/records/UserRecordsTest.java
+++ src/test/java/alt/sim/model/user/records/UserRecordsTest.java
@@ -6,10 +6,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import alt.sim.model.user.UserImpl;
 import alt.sim.model.user.records.RecordsFolder.RecordsPath;
+import alt.sim.model.user.validation.RecordsValidation;
 
 public class UserRecordsTest {
 
@@ -19,16 +22,20 @@ public class UserRecordsTest {
       private UserRecordsImpl userRecords = new UserRecordsImpl();
       private static final Path USER_RECORDS_FILE_PATH = Path.of(RecordsPath.USER_RECORDS_FILE_PATH.getPath());
 
-      @Test
-      public void initialize() {
+      // static modifier REQUIRED by BeforeAll tag;
+      // to remove static, should annotate class with tag
+      // TestInstance(Lifecycle.PER_CLASS)
+      @BeforeAll
+      public static void initialize() throws IOException {
           if (Files.exists(USER_RECORDS_FILE_PATH)) {
               System.out.println("File exists");
           } else {
-              System.out.println("File does not exist");
+              System.out.println("File does not exist. I'm creating it now for you!");
+              new RecordsValidation().userRecordsFileValidation();
           }
       }
 
-      @Test
+      @BeforeEach
       public void add() throws IOException {
           this.userRecords.addUser(new UserImpl(user1, 0));
           this.userRecords.addUser(new UserImpl(user2, 0));
