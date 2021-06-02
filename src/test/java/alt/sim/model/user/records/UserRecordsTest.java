0eb7e414cc93ed7f7860e5663a5363900f8180b1
Andrea <andrea.severi12@studio.unibo.it>
2021-06-02 10:10:27 +0200

    update with simple tests

--- src/test/java/alt/sim/model/user/records/UserRecordsTest.java
+++ src/test/java/alt/sim/model/user/records/UserRecordsTest.java
@@ -2,8 +2,9 @@ package alt.sim.model.user.records;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,15 +13,29 @@ import alt.sim.model.user.records.RecordsFolder.RecordsPath;
 
 public class UserRecordsTest {
 
-    private final String user1 = "Paolo";
-    private final String user2 = "Luca";
-    private UserRecordsImpl userRecords;
-    private static final Path USERS_DATA_FILE_PATH = Path.of(RecordsPath.USER_RECORDS_DIR_PATH.getPath());
 
-    @Test
-    public void testJson() throws Exception {
-        this.userRecords.addUser(new UserImpl(this.user1, 100));
-        assertEquals(this.userRecords.getUserByName(user1), Optional.of(new UserImpl(user1, 100)));
-    }
+      private final String user1 = "Paolo";
+      private final String user2 = "Luca";
+      private UserRecordsImpl userRecords = new UserRecordsImpl();
+      private static final Path USER_RECORDS_FILE_PATH = Path.of(RecordsPath.USER_RECORDS_FILE_PATH.getPath());
 
+      @Test
+      public void initialize() {
+          if (Files.exists(USER_RECORDS_FILE_PATH)) {
+              System.out.println("File exists");
+          } else {
+              System.out.println("File does not exist");
+          }
+      }
+
+      @Test
+      public void add() throws IOException {
+          this.userRecords.addUser(new UserImpl(user1, 0));
+          this.userRecords.addUser(new UserImpl(user2, 0));
+      }
+
+      @Test
+      public void isNameTaken() throws IOException {
+          assertEquals(true, this.userRecords.isPresent(user1));
+      }
 }
