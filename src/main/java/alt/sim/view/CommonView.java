86534851eade2f329bdd9bf5f4d1427f133cdf5a
Andrea <andrea.severi12@studio.unibo.it>
2021-06-02 18:01:30 +0200

    minor fix

--- src/main/java/alt/sim/view/CommonView.java
+++ src/main/java/alt/sim/view/CommonView.java
@@ -5,11 +5,9 @@ import java.io.IOException;
 import alt.sim.Main;
 import alt.sim.view.pages.Page;
 import alt.sim.view.pages.PageLoader;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -20,16 +18,13 @@ public final class CommonView {
 
     private CommonView() { }
 
-    @FXML
     public static void goBack() {
         PAGE_LOADER.loadPage(Main.getStage(), Page.HOME);
     }
 
-    @FXML
     public static void onPauseClick() throws IOException {
         Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/PauseDialog.fxml"));
         POPUP_STAGE.setScene(new Scene(root));
-        POPUP_STAGE.initModality(Modality.APPLICATION_MODAL);
         POPUP_STAGE.showAndWait();
     }
 }
