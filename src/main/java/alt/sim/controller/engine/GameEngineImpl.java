e77d047869c0aa62ce17baf5d2c2338a88ef3ee3
Daniel Rodilosso <daniel.rodilosso@outlook.com>
2021-06-10 08:37:28 +0200

    Resolved conflicts in GameEngineImpl.java

--- src/main/java/alt/sim/controller/engine/GameEngineImpl.java
+++ src/main/java/alt/sim/controller/engine/GameEngineImpl.java
@@ -3,8 +3,12 @@ package alt.sim.controller.engine;
 import alt.sim.controller.spawn.SpawnObject;
 import alt.sim.controller.spawn.SpawnObjectImpl;
 import alt.sim.view.PlaneMouseMove;
+import javafx.animation.PathTransition;
 import javafx.geometry.Point2D;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.util.Duration;
 
 public class GameEngineImpl implements GameEngine {
 
@@ -12,16 +16,50 @@ public class GameEngineImpl implements GameEngine {
     private SpawnObject spawn;
     private PlaneMouseMove plane;
     private Point2D[] vet;
-    private boolean start;
-    private int cont;
+    private Point2D[] coordinatesTest;
+
+    private PathTransition pathTransition;
     private Path path = new Path();
 
+    private int cont;
+    private boolean start;
+
     public GameEngineImpl(final PlaneMouseMove plane) {
+        this.pathTransition = new PathTransition();
         spawn = new SpawnObjectImpl();
         this.plane = plane;
+
         start = false;
         cont = 0;
+
+        path.getElements().add(new MoveTo(0, 0));
+
+        this.vet = this.plane.getPlaneMovement().getPlaneCoordinates();
+
+        pathTransition.setNode(plane.getPlane().getImagePlane());
+        pathTransition.setOrientation(PathTransition.OrientationType.NONE);
+        pathTransition.setDuration(Duration.millis(3000));
+
+        // TEST adding static coordinates
+        coordinatesTest = new Point2D[] {
+                new Point2D(0, 0),
+                new Point2D(196, 60),
+                new Point2D(341, 138),
+                new Point2D(601, 369),
+                new Point2D(773, 119),
+                new Point2D(910, 161),
+                new Point2D(953, 123),
+                new Point2D(1144, 50),
+                new Point2D(1600, 1500),
+        };
+
+        // TEST insert the static coordinates into path
+        for (int i = 1; i < coordinatesTest.length; i++) {
+            path.getElements().add(new LineTo(coordinatesTest[i].getX(), coordinatesTest[i].getY()));
+        }
+
     }
+
     @Override
     public void initGame() {
         spawn.startSpawn();
@@ -30,12 +68,15 @@ public class GameEngineImpl implements GameEngine {
     @Override
     public void mainLoop() throws IllegalArgumentException {
         long lastTime = System.currentTimeMillis();
+
         while (true) {
             long current = System.currentTimeMillis();
             int elapsed = (int) (current - lastTime);
+
             processInput();
             update(elapsed);
             render();
+
             try {
                 waitForNextFrame(current);
             } catch (IllegalArgumentException e) {
@@ -43,9 +84,11 @@ public class GameEngineImpl implements GameEngine {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
             lastTime = current;
         }
     }
+
     /**
      * Calculates how many milliseconds has to wait for next frame.
      * @param current
@@ -54,6 +97,7 @@ public class GameEngineImpl implements GameEngine {
      */
     protected void waitForNextFrame(final long current) throws InterruptedException, IllegalArgumentException {
         long dt = System.currentTimeMillis() - current;
+
         if (dt < PERIOD) {
             try {
                 Thread.sleep(PERIOD - dt);
@@ -67,30 +111,101 @@ public class GameEngineImpl implements GameEngine {
 
     @Override
     public void processInput() {
+
         if (start) {
             this.vet = this.plane.getPlaneMovement().getPlaneCoordinates();
             /*for (int i = 0; i < vet.length; i++) {
                 path.getElements().add(new LineTo(vet[i].getX(), vet[i].getY()));
             }*/
         }
+
     }
 
     @Override
     public void update(final int elapsed) {
         if (start) {
+
             if (cont < this.vet.length) {
                 plane.startTransiction(vet[cont].getX(), vet[cont].getY());
                 cont++;
             }
+
+            // Method Path with AnimationTransition
+            //pathTransition.setPath(path); 
+            //pathTransition.play();
+
+            // Method Path inserted manually with rotation
+            double angleInclination = plane.getAngleInclination(coordinatesTest[cont], coordinatesTest[cont + 1]);
+
+            plane.getPlane().getImagePlane().setRotate(angleInclination);
+
+            //System.out.println("coordinatesTest[cont + 1]: " + coordinatesTest[cont + 1].getX() + " , " + coordinatesTest[cont + 1].getY());
+            plane.getPlane().getImagePlane().setLayoutX(coordinatesTest[cont + 1].getX());
+            plane.getPlane().getImagePlane().setLayoutY(coordinatesTest[cont + 1].getY());
+
+            System.out.println("layout X && Y: " + plane.getPlane().getImagePlane().getLayoutX() + " , " + plane.getPlane().getImagePlane().getLayoutY());
+
+            // si può anche inserire come Point2D final coordinatesTest[cont], sembra funzionare bene uguale
+            plane.centerImagePositionInGame(plane.getPlane(), coordinatesTest[cont]);
+
+            System.out.println("after centerd X && Y" + plane.getPlane().getImagePlane().getLayoutX() + " , " + plane.getPlane().getImagePlane().getLayoutY());
+
+            //plane.getPlane().getImagePlane().setLayoutX(plane.getPlaneHeadPosition(plane.getPlane()).getX());
+            //plane.getPlane().getImagePlane().setLayoutY(plane.getPlaneHeadPosition(plane.getPlane()).getY());
+
+            cont++;
+
         }
     }
 
     @Override
     public void render() {
 
+        /*
+         * if (start) { double x = getLineTo(cont).getX(); double y =
+         * getLineTo(cont).getY();
+         * 
+         * start = false; Path path = new Path();
+         * 
+         * if (this.vet[cont] != null && this.vet[cont + 1] != null) {
+         * path.getElements().add(new MoveTo(vet[cont].getX(), vet[cont].getY()));
+         * path.getElements().add(getLineTo(cont + 1));
+         * 
+         * 
+         * 
+         * 
+         * 
+         * System.out.println("Vet value " + vet[cont].getX() + " , " +
+         * vet[cont].getY()); System.out.println("getLineTo " + getLineTo(cont + 1));
+         * 
+         * pathTransition.setPath(path); pathTransition.play(); cont++;
+         * 
+         * pathTransition.setOnFinished(finisch ->
+         * this.plane.getPlane().getImagePlane().setLayoutX(x));
+         * pathTransition.setOnFinished(finisch ->
+         * this.plane.getPlane().getImagePlane().setLayoutY(y));
+         * pathTransition.setOnFinished(finisch -> this.setStart(true));
+         *  } 
+         * }
+         */
+    }
+
+    public LineTo getLineTo(final int cont) {
+        return new LineTo(this.vet[cont].getX(), this.vet[cont].getY());
     }
 
     public void setStart(final boolean start) {
         this.start = start;
     }
+
+
+    public void setCoordinate(final Point2D[] vet) {
+        /*
+         * for (int i = 0; i < vet.length; i++) {
+         * System.out.println("vet in GameEngine: " + vet[i]); }
+         */
+
+        this.vet = vet;
+    }
+
 }
