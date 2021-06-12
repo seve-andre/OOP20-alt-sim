17a5bb2ac7b70a1d8a18b9821600220915df8825
Daniel Rodilosso <daniel.rodilosso@outlook.com>
2021-06-12 11:33:12 +0200

    Removing glut method and implements Explosion animation

--- src/main/java/alt/sim/model/ExplosionAnimation.java
+++ src/main/java/alt/sim/model/ExplosionAnimation.java
@@ -1,102 +1,66 @@
 package alt.sim.model;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
+import alt.sim.model.plane.Plane;
+import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.shape.Circle;
-import javafx.util.Duration;
 
 public class ExplosionAnimation {
-
+    private final static int NUMBER_IMAGES_ANIMATION = 67;
+    
     private List<Image> images;
-    private ImageView imageView;
+    private Plane planeSprite;
     private int contImages;
 
     public ExplosionAnimation() {
-        this.imageView = new ImageView();
         this.images = new ArrayList<Image>();
         this.contImages = 0;
 
-        this.images.add(new Image("images/animations/explosion_1.png"));
-        this.images.add(new Image("images/animations/explosion_2.png"));
-        this.images.add(new Image("images/animations/explosion_3.png"));
-        this.images.add(new Image("images/animations/explosion_4.png"));
-        this.images.add(new Image("images/animations/explosion_5.png"));
-        this.images.add(new Image("images/animations/explosion_6.png"));
-        this.images.add(new Image("images/animations/explosion_7.png"));
+        for (int i = 1; i < NUMBER_IMAGES_ANIMATION; i++) {
+            this.images.add(new Image("images/animations/explosion_" + i + ".png"));
+        }
+
+
     }
 
-    public void getExplosionAnimation(final ImageView imageView) throws IOException {
-        this.imageView = imageView;
+    public ExplosionAnimation(final Plane planeSprite) {
+        this();
 
+        this.planeSprite = planeSprite;
+    }
 
-            for (contImages = 0; contImages < images.size();) {
+    public AnimationTimer getExplosionAnimation() {
 
-                Thread thread = new Thread(new Runnable() {
- 
-                    @Override
-                    public void run() {
-                        Runnable updater = new Runnable() {
+        class MyTimer extends AnimationTimer {
 
-                            @Override
-                            public void run() {
-                                try {
-                                    Thread.sleep(2000);
-                                } catch (InterruptedException e) {
-                                    // TODO Auto-generated catch block
-                                    e.printStackTrace();
-                                }
+            @Override
+            public void handle(long now) {
+                try {
+                    startingExplosionAniamtion();
+                } catch (InterruptedException e) { e.printStackTrace(); }
+            }
 
-                                System.out.println("dentro RUN" + contImages);
-                                if (contImages < images.size()) {
-                                    System.out.println("dentro if: " + contImages);
-                                    imageView.setImage(images.get(contImages));
-                                    contImages++;
-                                    
-                                }
-                            }
-                        };
+            private void startingExplosionAniamtion() throws InterruptedException {
 
-                        while (true) {
-                            try {
-                                Thread.sleep(2000);
-                            } catch (InterruptedException ex) {
-                            }
+                Platform.runLater(new Runnable() {
 
-                            // UI update is run on the Application thread
-                            Platform.runLater(updater);
+                    @Override public void run() {
+                        if (contImages < images.size()) {
+                            planeSprite.getImagePlane().setImage(images.get(contImages));
+                            contImages++;
+                        } else {
+                            System.out.println("Stop Explosion Animation");
+                            stop();
                         }
                     }
                 });
-                // don't let thread prevent JVM shutdown
-                thread.setDaemon(true);
-                thread.start();
             }
-    }
+        }
 
-    public void getExplosionAnimation() {
-        /*
-         * Timeline timeline = new Timeline(); Image image = new Image(""); ImageView
-         * sprite = new ImageView();
-         * 
-         * timeline.getKeyFrames().addAll( new KeyFrame(Duration.ZERO, // set start
-         * position at 0 new KeyValue(sprite.imageProperty().setValue(), 100), new
-         * KeyValue(circle.translateYProperty(), 200)),
-         * 
-         * new KeyFrame(new Duration(40000), // set end position at 40s new
-         * KeyValue(circle.translateXProperty(), 100), new
-         * KeyValue(circle.translateYProperty(), 200)));
-         * 
-         * // play 40s of animation timeline.play();
-         */
+        return new MyTimer();
     }
 
 }
