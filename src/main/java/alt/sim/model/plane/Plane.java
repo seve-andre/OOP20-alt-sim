20dcd9a6130a9bf4e1a4afdecad0428c3633a82e
Daniel Rodilosso <daniel.rodilosso@outlook.com>
2021-06-16 22:47:36 +0200

    Removed getExplosionAnimation() from Plane.java

--- src/main/java/alt/sim/model/plane/Plane.java
+++ src/main/java/alt/sim/model/plane/Plane.java
@@ -1,16 +1,11 @@
 package alt.sim.model.plane;
 
-import java.io.IOException;
-
-import alt.sim.model.ExplosionAnimation;
 import alt.sim.model.ImageClassification;
 import alt.sim.model.LandingAnimation;
 import alt.sim.model.calculation.Sprite;
 import javafx.animation.ScaleTransition;
-import javafx.animation.Timeline;
 import javafx.geometry.Point2D;
 import javafx.scene.image.ImageView;
-import javafx.util.Duration;
 
 /**
  * Defines the Plane idea
@@ -34,16 +29,13 @@ public class Plane {
     private Sprite spritePlane;
 
     // Section Plane-Animation:
-    private ExplosionAnimation explosionAnimation;
     private LandingAnimation landingAnimation;
-
 
     public Plane(final String urlImagePlane) {
        this.spritePlane = new Sprite(urlImagePlane, true);
 
        // Initialize Animation
-       this.explosionAnimation = new ExplosionAnimation(new Point2D(20, 50));
-       this.landingAnimation = new LandingAnimation(new Point2D(20, 50), this.getImagePlane());
+       this.landingAnimation = new LandingAnimation(this.getImagePlane());
     }
 
     public Plane(final ImageClassification imageClassification) {
@@ -80,10 +72,6 @@ public class Plane {
 
     public ScaleTransition getLandingAnimation() {
         return landingAnimation.getLandingAnimation();
-    }
-
-    public void getExplosionAnimation() throws IOException {
-        explosionAnimation.getExplosionAnimation().start();
     }
 
     public void setPlaneRotate(final double rotateValue) {
