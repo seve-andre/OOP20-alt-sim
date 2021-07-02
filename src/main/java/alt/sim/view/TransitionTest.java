149e5a8f8c7d3a36c65d4c5c49f0ac9ddbb7a633
Daniel Rodilosso <daniel.rodilosso@outlook.com>
2021-07-02 08:56:46 +0200

    Update TransitionTest.java

--- src/main/java/alt/sim/view/TransitionTest.java
+++ src/main/java/alt/sim/view/TransitionTest.java
@@ -6,55 +6,50 @@ import java.util.List;
 import java.util.Set;
 
 import alt.sim.controller.engine.GameEngineAreaTest;
-import alt.sim.model.ImageClassification;
 import alt.sim.model.PlaneMovement;
 import alt.sim.model.plane.Plane;
 import javafx.animation.PathTransition;
-import javafx.animation.PathTransition.OrientationType;
 import javafx.animation.Animation.Status;
 import javafx.application.Application;
-import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class TransitionTest extends Application {
+    private Pane paneRoot;
     private PlaneMovement planeMove;
     private List<Plane> planes;
     private Plane plane;
     private Plane plane2;
+    private Plane plane3;
+    private Plane plane4;
     private Canvas canvas;
 
     private GameEngineAreaTest engine;
     private PathTransition pathTransition;
     private Path path;
     private List<Point2D> planeCoordinates;
-    private List<LineTo> lineToClearing;
+    private List<Line> linesPath;
     private GraphicsContext gc;
-    
-    private boolean isReadyToClear;
 
     // Area Collision
     private Rectangle rectangleWall;
 
     @Override
     public void start(final Stage stage) throws Exception {
-        Pane paneRoot = new Pane();
+        paneRoot = new Pane();
         canvas = new Canvas(MainPlaneView.getScreenWidth(), MainPlaneView.getScreenHeight());
         engine = new GameEngineAreaTest(this);
 
@@ -62,32 +57,47 @@ public class TransitionTest extends Application {
         path = new Path();
 
         planeCoordinates = new ArrayList<Point2D>();
-        lineToClearing = new ArrayList<LineTo>();
         planeMove = new PlaneMovement();
+        linesPath = new ArrayList<>();
 
         planes = new ArrayList<>();
         plane = new Plane("images/map_components/airplane.png");
         plane2 = new Plane("images/map_components/airplane.png");
+        plane3 = new Plane("images/map_components/airplane.png");
+        plane4 = new Plane("images/map_components/airplane.png");
+
+        plane.connetToController(this);
+        plane2.connetToController(this);
+        plane3.connetToController(this);
+        plane4.connetToController(this);
         planes.add(plane);
         planes.add(plane2);
+        planes.add(plane3);
+        planes.add(plane4);
 
         rectangleWall = new Rectangle(800, 600, 40, 30);
-
-        isReadyToClear = false;
         // Inizio funzionamento della View
 
         // Calculating the Proportion --> (Image:Screen)
         plane.getSpritePlane().getImageSpriteResized().resizeImageSprite(true);
         plane2.getSpritePlane().getImageSpriteResized().resizeImageSprite(true);
+        plane3.getSpritePlane().getImageSpriteResized().resizeImageSprite(true);
+        plane4.getSpritePlane().getImageSpriteResized().resizeImageSprite(true);
+        
         // View Plane demonstrating:
         paneRoot.resize(MainPlaneView.getScreenWidth(), MainPlaneView.getScreenHeight());
 
         // Insert Plane test into view:
         plane2.getImagePlane().setX(500);
         plane2.getImagePlane().setY(500);
+        plane3.getImagePlane().setX(800);
+        plane3.getImagePlane().setY(100);
+        plane4.getImagePlane().setX(800);
+        plane4.getImagePlane().setY(800);
 
         // Section Canvas + WallCollision + ImageTest
         paneRoot.getChildren().addAll(canvas, rectangleWall, plane.getImagePlane(), plane2.getImagePlane());
+        paneRoot.getChildren().addAll(plane3.getImagePlane(), plane4.getImagePlane());
         gc = canvas.getGraphicsContext2D();
         gc.save();
 
@@ -108,13 +118,22 @@ public class TransitionTest extends Application {
         EventHandler<MouseEvent> handlerMouseDragged = event -> {
             if (planeCoordinates.size() < PlaneMovement.COORDINATES_LIMIT) {
                 planeCoordinates.add(new Point2D(event.getX(), event.getY()));
-                gc.lineTo(event.getX(), event.getY());
-                gc.setStroke(Color.LIMEGREEN);
-                gc.stroke();
+                //gc.lineTo(event.getX(), event.getY());
+                //gc.setStroke(Color.LIMEGREEN);
+                //gc.stroke();
             }
         };
 
         EventHandler<MouseEvent> handlerMouseReleased = event -> {
+            convertCoordinatesToLine();
+
+            for (Plane planeSelected:planes) {
+
+                if (planeSelected.getIsPlaneSelectedForBeenMoved()) {
+                    paneRoot.getChildren().addAll(planeSelected.getPlaneLinesPath());
+                }
+            }
+
             // 2)
             // Blocca il ciclo in GameEngineImpl per resettare le nuove coordinate.
             if (pathTransition.getStatus() == Status.RUNNING) {
@@ -126,11 +145,11 @@ public class TransitionTest extends Application {
             planeMove.setPlaneCoordinatesList(clearListCoordinates(planeCoordinates));
             copyCoordinatesInPath(planeCoordinates);
             selectedPathNode();
+            clearPlaneSelectedForBeenMoved();
             //connectPlaneToPathTransition();
 
             // Quando viene rilasciato il Mouse le coordinate salvate vengono liberate
             planeCoordinates.clear();
-
         };
 
         canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, handlerMouseReleased);
@@ -142,6 +161,27 @@ public class TransitionTest extends Application {
         stage.show();
     }
 
+    private void convertCoordinatesToLine() {
+        for (int i = 0; i < planeCoordinates.size(); i++) {
+            try {
+                if (planeCoordinates.get(i + 1) != null) {
+                    linesPath.add(new Line(planeCoordinates.get(i).getX(), planeCoordinates.get(i).getY(), planeCoordinates.get(i + 1).getX(), planeCoordinates.get(i + 1).getY()));
+                }
+            } catch (Exception e) {};
+        }
+
+        for (Plane planeSelected:planes) {
+
+            if (planeSelected.getIsPlaneSelectedForBeenMoved()) {
+                clearingLinesPathToPaneRoot(planeSelected.getPlaneLinesPath());
+                planeSelected.setPlaneLinesPath(linesPath);
+            }
+        }
+
+        paneRoot.getChildren().addAll(linesPath);
+        linesPath.clear();
+    }
+
     private void selectedPathNode() {
         for (Plane planeSelected:planes) {
 
@@ -149,10 +189,22 @@ public class TransitionTest extends Application {
                 engine.setCoordinates(planeCoordinates);
                 engine.setPathTransition(pathTransition);
                 engine.setReadyToStart(true);
+                engine.setPlane(planeSelected);
                 engine.connectPlaneToPathTransition(plane);
                 planeSelected.setIsPlaneSelectedForBeenMoved(false);
             } 
+            planeSelected.setIsPlaneSelectedForBeenMoved(false);
         }
+    }
+
+    public void clearPlaneSelectedForBeenMoved() {
+        for (Plane planeSelected:planes) {
+            planeSelected.setIsPlaneSelectedForBeenMoved(false);
+        }
+    }
+
+    public void clearingLinesPathToPaneRoot(final List<Line> linesPathToRemove) {
+        paneRoot.getChildren().remove(linesPathToRemove);
     }
 
     /*
@@ -163,21 +215,6 @@ public class TransitionTest extends Application {
      * pathTransitionPlane.setPath(path);
      * pathTransitionPlane.setNode(plane.getImagePlane());
      * pathTransitionPlane.setOrientation(OrientationType.ORTHOGONAL_TO_TANGENT);
-     * 
-     * // ------------------------------------------- // Cambiare la velocità a
-     * seconda del percoso: pathLenght = planeCoordinates.size();
-     * 
-     * if (pathLenght <= 10) {
-     * pathTransitionPlane.setDuration(Duration.millis(100)); } else if (pathLenght
-     * > 10 && pathLenght <= 50) {
-     * pathTransitionPlane.setDuration(Duration.millis(500)); } else if (pathLenght
-     * > 50 && pathLenght <= 100){
-     * pathTransitionPlane.setDuration(Duration.millis(1000)); } else if (pathLenght
-     * > 100 && pathLenght <= 150){
-     * pathTransitionPlane.setDuration(Duration.millis(1500)); } else if (pathLenght
-     * > 150 && pathLenght <= 200){
-     * pathTransitionPlane.setDuration(Duration.millis(2000)); } //
-     * -------------------------------------------
      * 
      * pathTransitionPlane.play();
      * 
