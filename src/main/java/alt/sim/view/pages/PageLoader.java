a3b60b9ee92b1cf85f5c615da4c825da9bad2312
Andrea <andrea.severi12@studio.unibo.it>
2021-07-15 17:33:59 +0200

    remove redundant stage parameter

--- src/main/java/alt/sim/view/pages/PageLoader.java
+++ src/main/java/alt/sim/view/pages/PageLoader.java
@@ -2,22 +2,20 @@ package alt.sim.view.pages;
 
 import java.io.IOException;
 
+import alt.sim.Main;
 import alt.sim.view.mapchoice.GameMap;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class PageLoader {
 
     /**
      * Loads page given as argument.
-     *
-     * @param stage to whom load new content
-     * @param page to load
+     *  @param page to load
      * @param gameMap to load
      */
-    public void loadPage(final Stage stage, final Page page, final GameMap gameMap) {
+    public void loadPage(final Page page, final GameMap gameMap) {
 
         /**
          * If given page is a GameMap, gets its name.
@@ -34,17 +32,15 @@ public class PageLoader {
             e.printStackTrace();
         }
 
-        stage.setScene(new Scene(root));
+        Main.getStage().setScene(new Scene(root));
     }
-
 
     /**
      * Method overloading.
      *
-     * @param stage to whom load new content
      * @param page to load
      */
-    public void loadPage(final Stage stage, final Page page) {
-        this.loadPage(stage, page, null);
+    public void loadPage(final Page page) {
+        this.loadPage(page, null);
     }
 }
