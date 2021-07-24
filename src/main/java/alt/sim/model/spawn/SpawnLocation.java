94959854632408b33afe28845c3bfb677d6937d3
Andrea <andrea.severi12@studio.unibo.it>
2021-07-24 12:32:57 +0200

    add random spawn location method to enum

--- src/main/java/alt/sim/model/spawn/SpawnLocation.java
+++ src/main/java/alt/sim/model/spawn/SpawnLocation.java
@@ -1,8 +1,20 @@
 package alt.sim.model.spawn;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public enum SpawnLocation {
     LEFT,
     TOP,
     RIGHT,
-    BOTTOM
+    BOTTOM;
+
+    /**
+     * Gets a random SpawnLocation from values.
+     * @return SpawnLocation randomly between values
+     */
+    public static SpawnLocation getRandomSpawnLocation() {
+        final SpawnLocation[] values = values();
+        int index = ThreadLocalRandom.current().nextInt(values.length);
+        return values[index];
+    }
 }
