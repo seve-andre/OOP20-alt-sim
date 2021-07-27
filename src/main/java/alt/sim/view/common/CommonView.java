bfc23074adc49b6efc6c71452ddc74d0a20070a2
Andrea <andrea.severi12@studio.unibo.it>
2021-07-27 18:15:47 +0200

    add close and minimize methods and change package

--- src/main/java/alt/sim/view/common/CommonView.java
+++ src/main/java/alt/sim/view/common/CommonView.java
@@ -1,18 +1,19 @@
-package alt.sim.view;
+package alt.sim.view.common;
 
-import java.io.IOException;
-
+import alt.sim.Main;
 import alt.sim.view.pages.Page;
 import alt.sim.view.pages.PageLoader;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.IOException;
+
 public final class CommonView {
 
-    private static final Stage POPUP_STAGE = new Stage(StageStyle.UNDECORATED);
 
     private CommonView() {
     }
@@ -21,9 +22,19 @@ public final class CommonView {
         PageLoader.loadPage(Page.HOME);
     }
 
-    public static void onPauseClick() throws IOException {
+    public static void pause() throws IOException {
+        Stage POPUP_STAGE = new Stage(StageStyle.UNDECORATED);
         Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/PauseDialog.fxml"));
         POPUP_STAGE.setScene(new Scene(root));
         POPUP_STAGE.showAndWait();
+    }
+    
+    public static void minimize() {
+        Main.getStage().setIconified(true);
+    }
+
+    public static void close() {
+        Platform.exit();
+        System.exit(0);
     }
 }
