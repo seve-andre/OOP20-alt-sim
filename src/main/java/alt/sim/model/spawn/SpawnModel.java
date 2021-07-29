637982b6d2835037bdb476e70fe057570ff24b63
Andrea <andrea.severi12@studio.unibo.it>
2021-07-29 09:35:40 +0200

    spawn model refactoring (still not the best)

--- src/main/java/alt/sim/model/spawn/SpawnModel.java
+++ src/main/java/alt/sim/model/spawn/SpawnModel.java
@@ -1,30 +1,33 @@
 package alt.sim.model.spawn;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import alt.sim.model.map.MapImpl;
+import alt.sim.Main;
 import alt.sim.model.plane.Plane;
 import alt.sim.model.plane.State;
+import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
 import javafx.animation.PathTransition;
-import javafx.scene.shape.CubicCurveTo;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class SpawnModel {
 
     private static final int PATH_TRANSITION_DURATION = 5000;
-    private static final int X = 80;
-    private static final int Y = 80;
+    private static final int X = 50;
+    private static final int Y = 50;
 
-    private static final MapImpl MAP = new MapImpl();
-
-    private static final int HEIGHT = MAP.getHeight();
-    private static final int WIDTH = MAP.getWidth();
-    private static final int HEIGHT_2 = HEIGHT / 2;
-    private static final int WIDTH_2 = WIDTH / 2;
+    private static final double HEIGHT = Main.getStage().getHeight();
+    private static final double WIDTH = Main.getStage().getWidth();
+    private static final double HEIGHT_2 = HEIGHT / 2;
+    private static final double WIDTH_2 = WIDTH / 2;
+    private static ImageView indicatorImage = new ImageView(new Image("images/animations/indicator.png"));
 
     private SpawnModel() { }
 
@@ -42,19 +45,24 @@ public final class SpawnModel {
         switch (spawnLocation) {
             case LEFT:
                 path.getElements().add(new MoveTo(-X, HEIGHT_2));
-                path.getElements().add(new CubicCurveTo(180, 0, 200, 120, 250, HEIGHT_2));
+                path.getElements().add(new LineTo(X, HEIGHT_2));
+                //path.getElements().add(new CubicCurveTo(50, 400, 90, 450, 130, 360));
+                loadIndicator(0, HEIGHT_2);
                 break;
             case TOP:
-                path.getElements().add(new MoveTo(WIDTH_2, Y));
-                path.getElements().add(new LineTo(WIDTH_2, 200));
+                path.getElements().add(new MoveTo(WIDTH_2, -Y));
+                path.getElements().add(new LineTo(WIDTH_2, Y));
+                loadIndicator(WIDTH_2, 0);
                 break;
             case RIGHT:
                 path.getElements().add(new MoveTo(WIDTH + X, HEIGHT_2));
-                path.getElements().add(new LineTo(X, HEIGHT_2));
+                path.getElements().add(new LineTo(WIDTH - X, HEIGHT_2));
+                loadIndicator(WIDTH, HEIGHT_2);
                 break;
             case BOTTOM:
                 path.getElements().add(new MoveTo(WIDTH_2, HEIGHT + Y));
-                path.getElements().add(new LineTo(WIDTH_2, Y));
+                path.getElements().add(new LineTo(WIDTH_2, HEIGHT - Y));
+                loadIndicator(WIDTH_2, HEIGHT);
                 break;
             default:
                 break;
@@ -67,6 +75,10 @@ public final class SpawnModel {
         pathTransition.setOnFinished(event -> plane.setState(State.WAITING));
 
         return pathTransition;
+    }
+
+    public PathTransition showImage(final ImageView indicator) {
+        return null;
     }
 
     /**
@@ -86,5 +98,38 @@ public final class SpawnModel {
         }
 
         return planes;
+    }
+
+    /**
+     * Generates list of 4 different indicators.
+     * @return list containing indicators
+     */
+    public static List<ImageView> generateIndicators() {
+
+        List<ImageView> indicators = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            ImageView indicatorImage = new ImageView(new Image("images/animations/indicator.png"));
+            indicators.add(indicatorImage);
+        }
+
+        return indicators;
+    }
+
+    public static void loadIndicator(final double x, final double y) {
+
+        Timeline indicatorTimer = new Timeline(new KeyFrame(Duration.seconds(1), cycle -> {
+            FadeTransition indicatorTransition = new FadeTransition();
+            indicatorTransition.setNode(indicatorImage);
+            indicatorTransition.setDuration(Duration.seconds(1));
+            indicatorTransition.setFromValue(1);
+            indicatorTransition.setToValue(0);
+            indicatorTransition.play();
+        }));
+
+        indicatorImage.setX(x);
+        indicatorImage.setY(y);
+        indicatorTimer.setCycleCount(2);
+        indicatorTimer.play();
     }
 }
