74fffdf7b1d72cc25cc0aa0418751d2b3cc20908
Daniel Rodilosso <daniel.rodilosso@outlook.com>
2021-08-17 11:26:05 +0200

    Update section Animation

--- src/main/java/alt/sim/model/animation/AnimationPlane.java
+++ src/main/java/alt/sim/model/animation/AnimationPlane.java
@@ -1,14 +1,12 @@
 package alt.sim.model.animation;
 
-import javafx.geometry.Point2D;
-
 /**
  *  Define the Animation class behavior.
  */
-public abstract class AnimationPlane {
-    protected AnimationPlane() { }
+public interface AnimationPlane {
 
-    public abstract void startAnimation();
-
-    public abstract void setPositionAnimation(Point2D positionAnimation);
+    /**
+     * intializa the animation and starting it.
+     */
+    void start();
 }
--- src/main/java/alt/sim/model/animation/LandingAnimation.java
+++ src/main/java/alt/sim/model/animation/LandingAnimation.java
@@ -1,64 +1,19 @@
 package alt.sim.model.animation;
 
-
-import javafx.animation.ScaleTransition;
-import javafx.geometry.Point2D;
 import javafx.scene.image.ImageView;
-import javafx.util.Duration;
 
 /**
  * Create LandingAnimation for implement ScaleTransition animation when the Plane Landing in the Airstrip.
  */
-public class LandingAnimation extends AnimationPlane {
-    private static final int DURATION_LANDING_ANIMATION = 1000;
-
-    private ImageView spriteToApplyAnimation;
-    private ScaleTransition landingAnimation;
+public class LandingAnimation extends Animation implements AnimationPlane {
 
     public LandingAnimation(final ImageView spriteToApplyAnimation) {
-        this.spriteToApplyAnimation = spriteToApplyAnimation;
-        this.landingAnimation = new ScaleTransition();
-    }
-
-    /**
-     * Set the TransitionAnimation with right values.
-     */
-    private void settingLandingAnimation() {
-        //Setting the duration for the transition 
-        landingAnimation.setDuration(Duration.millis(DURATION_LANDING_ANIMATION)); 
-
-        //Setting the node for the transition 
-        landingAnimation.setNode(this.spriteToApplyAnimation); 
-
-        //Setting the final dimensions for scaling 
-        landingAnimation.setToX(0);
-        landingAnimation.setToY(0); 
-
-        //Setting the cycle count for the translation 
-        landingAnimation.setCycleCount(1); 
-
-        //Setting auto reverse value to true 
-        landingAnimation.setAutoReverse(false); 
-    }
-
-    /**
-     * @return LandingAnimation setted and ready to play.
-     */
-    public ScaleTransition getLandingAnimation() {
-        // Setting landingAnimation before return
-        settingLandingAnimation();
-
-        return landingAnimation;
+        super(spriteToApplyAnimation);
     }
 
     @Override
-    public void startAnimation() {
-        settingLandingAnimation();
+    public void start() {
+        settingDefaultAnimationOptions();
         getLandingAnimation().play();
-    }
-
-    @Override
-    public void setPositionAnimation(final Point2D positionAnimation) {
-        // TODO Auto-generated method stub
     }
 }
