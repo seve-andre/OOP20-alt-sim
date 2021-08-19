9896e97b506c9ec5bfbd5c5ab3239c8640655818
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-08-19 10:01:53 +0200

    Update SpriteType.java

--- src/main/java/alt/sim/model/sprite/SpriteType.java
+++ src/main/java/alt/sim/model/sprite/SpriteType.java
@@ -7,17 +7,17 @@ public enum SpriteType {
 
     /** Plane image path.*/
     AIRPLANE("images/map_components/airplane.png"),
-    /** Plane Selected image path */
+    /** Plane Selected image path. */
     AIRPLANE_SELECTED("images/map_components/airplaneSelected.png"),
     /** Airstrip image path.*/
-    AIRSTRIP( "images/map_components/singleAirstrip.png"),
+    AIRSTRIP("images/map_components/singleAirstrip.png"),
     /** Boat image path.*/
-    BOAT( "images/map_components/boat.png"),
+    BOAT("images/map_components/boat.png"),
     /** Helicopter image path.*/
     HELICOPTER("images/map_components/helicopter.png"),
     /** Helipad image path.*/
     HELIPAD("images/map_components/helipad.png"),
-    /** Indicator image path */
+    /** Indicator image path. */
     INDICATOR("images/map_components/indicator.png");
 
     private String urlImage;
