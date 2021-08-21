cd525bed65215e1023dc6016833a125368fc73fa
Andrea <andrea.severi12@studio.unibo.it>
2021-08-21 17:11:50 +0200

    remove field

--- src/main/java/alt/sim/controller/user/records/UserRecordsController.java
+++ src/main/java/alt/sim/controller/user/records/UserRecordsController.java
@@ -1,16 +1,14 @@
 package alt.sim.controller.user.records;
 
-import java.io.IOException;
-
 import alt.sim.model.user.records.UserRecordsImpl;
+
+import java.io.IOException;
 
 public final class UserRecordsController {
 
     private UserRecordsController() { }
 
-    private static final UserRecordsImpl USER_RECORDS = new UserRecordsImpl();
-
     public static void updateScore(final String name, final int score) throws IOException {
-        USER_RECORDS.updateScore(name, score);
+        new UserRecordsImpl().updateScore(name, score);
     }
 }
