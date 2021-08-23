89b7b6028a5cc41f49c1949ee69b307077d80bda
Andrea Foschi <andrea.foschi13@studio.unibo.it>
2021-08-23 15:40:51 +0200

    Create AirStripStatusTest.java

---
+++ src/test/java/alt/sim/model/airstrip/AirStripStatusTest.java
@@ -0,0 +1,30 @@
+package alt.sim.model.airstrip;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+import alt.sim.controller.airstrip.AirStripController;
+import alt.sim.controller.seaside.SeasideController;
+import alt.sim.model.plane.Plane;
+
+public class AirStripStatusTest {
+
+    private AbstractAirStrip strip = new BasicAirStrip();
+
+    private Plane plane1 = new Plane();
+    private Plane plane2 = new Plane();
+    //private Plane plane3 = new Plane();
+
+    private SeasideController transitionSeaside = new SeasideController();
+
+    @Test
+    public void checkStatus() {
+        strip.setStatus(AirStripStatus.BUSY);
+        assertFalse(AirStripController.acceptPlane(strip, transitionSeaside, plane1));
+        strip.setStatus(AirStripStatus.DISABLED);
+        assertFalse(AirStripController.acceptPlane(strip, transitionSeaside, plane2));
+        strip.setStatus(AirStripStatus.FREE);
+        //assertTrue(AirStripController.acceptPlane(strip, transitionSeaside, plane3));
+    }
+}
