32961e4d9ecb85e14a3c32f676616c1c03b7ea83
Andrea <andrea.severi12@studio.unibo.it>
2021-08-23 18:28:58 +0200

    remove home button

--- src/main/java/alt/sim/view/gameover/GameOverView.java
+++ src/main/java/alt/sim/view/gameover/GameOverView.java
@@ -1,19 +1,9 @@
 package alt.sim.view.gameover;
 
 import alt.sim.view.common.WindowView;
-import alt.sim.view.pages.Page;
-import alt.sim.view.pages.PageLoader;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
 public class GameOverView {
-
-    @FXML
-    public void onHomeClick(final ActionEvent event) {
-        PageLoader.loadPage(Page.HOME);
-        ((Button) event.getSource()).getScene().getWindow().hide();
-    }
 
     @FXML
     public void onQuitClick() {
